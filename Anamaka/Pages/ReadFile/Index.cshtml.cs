261788d0fb5958d58e99e089226feec279da7fd9
HG_PX <mr.hgpx@gmail.com>
2021-04-11 22:42:02 +0430

    Add project files.

---
+++ Anamaka/Pages/ReadFile/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Anamaka.Pages.ReadFile
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
